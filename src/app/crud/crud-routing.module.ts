0e5a859977b1dccabe58764f9b005f52acb0017f
guoika <phuongnt@consultindochina.com>
2021-12-14 16:15:54 +0700

    c

--- src/app/crud/crud-routing.module.ts
+++ src/app/crud/crud-routing.module.ts
@@ -4,12 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    // {
-    //     path: '',
-    //     component: LoginComponent,
-    // },
     {
         path: '',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
         component: RegisterComponent
     },
 
